b60ecd5d30f0eac839467db65a7799fc495fed7c
Edu58 <edumuriithi58@gmail.com>
2022-04-09 10:28:23 +0300

    adds quotes and toggle details

--- src/app/Components/quote/quote.component.ts
+++ src/app/Components/quote/quote.component.ts
@@ -1,5 +1,6 @@
 import { Quote } from 'src/app/model/quote.model';
 import { Component, OnInit } from '@angular/core';
+import { faMaximize, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-quote',
@@ -7,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  expand: IconDefinition = faMaximize
 
   constructor () { }
   
   quotes: Quote[] = [
-    new Quote( 'run', 'me', 'fun' ),
+    new Quote( 'run', 'mexcvg sdrgsergsfbsefgsvertgw r54 wtrtwrqt wrtw5tq4 tqrtq4 qt4', 'fun' ),
+    new Quote( 'dfd', 'sdasds', 'sdasd' ),
+    new Quote( 'run', 'mexcvg sdrgsergsfbsefgsvertgw r54 wtrtwrqt wrtw5tq4 tqrtq4 qt4', 'fun' ),
+    new Quote( 'dfd', 'sdasds', 'sdasd' ),
+    new Quote( 'run', 'mexcvg sdrgsergsfbsefgsvertgw r54 wtrtwrqt wrtw5tq4 tqrtq4 qt4', 'fun' ),
+    new Quote( 'dfd', 'sdasds', 'sdasd' ),
+    new Quote( 'run', 'mexcvg sdrgsergsfbsefgsvertgw r54 wtrtwrqt wrtw5tq4 tqrtq4 qt4', 'fun' ),
     new Quote('dfd', 'sdasds', 'sdasd')
   ]
 
